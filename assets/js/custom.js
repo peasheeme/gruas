ad1863f1a47e53720c98afc5e7e94a500441a3d1
Perla Holguin <peasheeme@gmail.com>
2018-10-19 10:44:25 -0500

    boton fb resposive y footer

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,26 +1,52 @@
- /* --------------------- Tabs!!--------------*/ 
+ /* --------------------- Tabs!!--------------*/
 
  $('.tabs').tabslet();
 
- /* --------------------- backstretch!!--------------*/ 
+ /* --------------------- backstretch!!--------------*/
 
-  /* --------------------- carousel --------------*/ 
-
-$('.owl-carousel').owlCarousel({
-    rtl:true,
-    loop:true,
-    margin:10,
-    nav:true,
-    responsive:{
-        0:{
-            items:1
+  /* --------------------- carousel --------------*/
+  var owl = $('.owl-carousel');
+		 owl.owlCarousel({
+	    loop:true,
+	    margin:10,
+	    autoplay:true,
+	    autoplayTimeout:2000,
+	    autoplayHoverPause:true,
+      responsiveClass:true,
+      responsive : {
+        1 : { items: 1,
+              nav: true
         },
-        600:{
-            items:3
+        600 : { items: 3 ,
+                nav: false
         },
-        1000:{
-            items:5
+        1000 : { items:5,
+            nav:true,
+            loop:false
         }
-    }
-})
+      }
+	   });
+		$('.play').on('click',function(){
+		    owl.trigger('play.owl.autoplay',[2000])
+		})
+		$('.stop').on('click',function(){
+		    owl.trigger('stop.owl.autoplay')
+		})
+ /*$("#owl-gruas").owlCarousel({
+   items:5,
+            navigation: true,
+            navigationText: ['<i class="fa fa-angle-left"></i>', '<i class="fa fa-angle-right"></i>'],
+            slideSpeed : 300,
+            paginationSpeed : 400,
+            autoPlay: 5000,
 
+
+        });*/
+
+
+/* --------------------- Simple text rotator--------------*/
+$(".rotate").textrotator({
+            animation: "dissolve",
+            separator: "|",
+            speed: 3000
+        });
